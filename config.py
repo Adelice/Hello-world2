e082099d2d18c1030f476f2a1b75e9545ec9787e
Adelice <nancytoniw@gmail.com>
2022-02-04 09:17:26 +0200

    Deployment

---
+++ config.py
@@ -0,0 +1,41 @@
+import os 
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nancy:adelice@localhost/watchlist'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    
+ 
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='e3204b852fcb4a1621b5c8dbebfec368'
+export SECRET_KEY='12345'
+export MAIL_USERNAME='adeliceumutoniwase@gmail.com'
+export MAIL_PASSWORD='moringaschool'
+
+python3 manage.py server
